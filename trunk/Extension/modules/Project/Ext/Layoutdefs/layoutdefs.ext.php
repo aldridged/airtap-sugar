178bd502f394a1b6b3a2979e09d35b8e0cf6a4ba
aldridged@gmail.com <aldridged@gmail.com@1b385842-5835-4f50-da08-b7594bb11436>
2014-05-29 13:07:53 +0000

    Adjust buttons on project subpanels to allow creation of cases and contacts.

---
+++ trunk/Extension/modules/Project/Ext/Layoutdefs/layoutdefs.ext.php
@@ -0,0 +1,10 @@
+<?php
+  $layout_defs["Project"]["subpanel_setup"]["cases"]["top_buttons"] = array(
+    array('widget_class' => 'SubPanelTopCreateButton'),
+	array('widget_class' => 'SubPanelTopSelectButton', 'popup_module' => 'Cases'),
+	);
+  $layout_defs["Project"]["subpanel_setup"]["contacts"]["top_buttons"] = array(
+    array('widget_class' => 'SubPanelTopCreateButton'),
+	array('widget_class' => 'SubPanelTopSelectButton', 'popup_module' => 'Contacts'),
+	);
+?>
