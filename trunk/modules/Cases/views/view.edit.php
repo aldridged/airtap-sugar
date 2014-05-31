528624f17b8b06112b2b0085e196b7f186cdcb37
aldridged@gmail.com <aldridged@gmail.com@1b385842-5835-4f50-da08-b7594bb11436>
2014-05-31 03:50:38 +0000

    Added Account id to Case inheritance from Project

--- trunk/modules/Cases/views/view.edit.php
+++ trunk/modules/Cases/views/view.edit.php
@@ -11,13 +11,22 @@ class CasesViewEdit extends ViewEdit{
 
   function display(){
     $this->ev->process();
-    echo "<!-- \n";
-    var_dump($_REQUEST);
-    echo "-->";
+    //echo "<!-- \n";
+    //var_dump($_REQUEST);
+	//var_dump($this->ev->fieldDefs);
+    //echo "-->";
     if (empty($this->ev->focus->id) && ($_REQUEST['return_relationship'] == 'projects_cases')) {
                         $parent = new Project();
                         $parent->retrieve($_REQUEST['return_id']);
-                        if((isset($parent->facility_c))&&($this->ev->fieldDefs['jobnumber_c']['value']=="")) { $this->ev->fieldDefs['jobnumber_c']['value'] = $parent->jobnumber_c; };  
+						$accts = $parent->get_linked_beans('accounts','Account');
+						$i=0;
+						$acctlist = array();
+						foreach($accts as $acct) {
+						  $acctlist[i] = $acct->id;
+						  $i++;
+						  };
+                        if((isset($parent->facility_c))&&($this->ev->fieldDefs['jobnumber_c']['value']=="")) { $this->ev->fieldDefs['jobnumber_c']['value'] = $parent->jobnumber_c; };
+						if(($i>0)&&($this->ev->fieldDefs['account_id']['value']=="")) { $this->ev->fieldDefs['account_id']['value'] = $acct->id; $this->ev->fieldDefs['account_name']['value'] = $acct->name;};
                 };
     echo $this->ev->display();
     }
