5518bce5b92da21990b5b61795714aae98e63c36
aldridged <aldridged@gmail.com>
2014-06-03 02:42:55 +0000

    Code to inherit account value from Case to Task

--- modules/Tasks/views/view.edit.php
+++ modules/Tasks/views/view.edit.php
@@ -13,11 +13,13 @@ class TasksViewEdit extends ViewEdit{
     $this->ev->process();
     echo "<!-- \n";
     var_dump($_REQUEST);
+    var_dump($this->ev->fieldDefs);
     echo "-->";
     if (empty($this->ev->focus->id) && ($_REQUEST['return_module'] == 'Cases')) {
           $parent = new aCase();
       $parent->retrieve($_REQUEST['return_id']);
-          if((isset($parent->jobnumber_c))&&($this->ev->fieldDefs['jobnumber_c']['value']=="")) { $this->ev->fieldDefs['jobnumber_c']['value'] = $parent->jobnumber_c; }; 
+          if((isset($parent->jobnumber_c))&&($this->ev->fieldDefs['jobnumber_c']['value']=="")) { $this->ev->fieldDefs['jobnumber_c']['value'] = $parent->jobnumber_c; };
+	if((isset($parent->account_id))&&($this->ev->fieldDefs['account_id_c']['value']=="")) { $this->ev->fieldDefs['account_id_c']['value'] = $parent->account_id; $this->ev->fieldDefs['account_c']['value'] = $parent->account_name; }; 
     }
     echo $this->ev->display();
     }
