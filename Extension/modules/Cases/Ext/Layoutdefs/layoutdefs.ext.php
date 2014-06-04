b0cb980f8a5b6b59ebc0854eac78347c9833a699
aldridged <aldridged@gmail.com>
2014-06-04 14:59:41 +0000

    Adjust Cases subpanel to use Full Form task create button

--- Extension/modules/Cases/Ext/Layoutdefs/layoutdefs.ext.php
+++ Extension/modules/Cases/Ext/Layoutdefs/layoutdefs.ext.php
@@ -1,5 +1,5 @@
 <?php
   $layout_defs["Cases"]["subpanel_setup"]["activities"]["top_buttons"] = array(
-    array('widget_class' => 'SubPanelTopCreateTaskButton'),
+    array('widget_class' => 'SubPanelTopCreateTaskFullButton'),
     );
 ?>
