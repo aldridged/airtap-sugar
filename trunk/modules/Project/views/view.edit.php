6273866b5278a3c319728445cd4e438ddec688d3
aldridged@gmail.com <aldridged@gmail.com@1b385842-5835-4f50-da08-b7594bb11436>
2014-06-11 02:04:43 +0000

    Adjusted inheritance code for accounts to project to take into account full form entry point

--- trunk/modules/Project/views/view.edit.php
+++ trunk/modules/Project/views/view.edit.php
@@ -13,9 +13,10 @@ class ProjectViewEdit extends ViewEdit{
     //echo "<!-- \n";
     //var_dump($_REQUEST);
     //echo "-->";
-    if (empty($this->ev->focus->id) && ($_REQUEST['relate_to'] == 'projects_accounts')) {
+    if (empty($this->ev->focus->id) && (($_REQUEST['relate_to'] == 'projects_accounts')||($_REQUEST['return_relationship'] == 'projects_accounts'))) {
       $parent = new Account();
-      $parent->retrieve($_REQUEST['relate_id']);
+      if(isset($_REQUEST['relate_id'])) { $parent->retrieve($_REQUEST['relate_id']); } 
+      else { $parent->retrieve($_REQUEST['parent_id']); };
 	  if((isset($parent->id))&&($this->ev->fieldDefs['account_id_c']['value']=="")) { $this->ev->fieldDefs['account_id_c']['value'] = $parent->id; $this->ev->fieldDefs['account_c']['value'] = $parent->name; };
     }
     echo $this->ev->display();
