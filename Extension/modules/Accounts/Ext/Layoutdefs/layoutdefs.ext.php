e3bfcf14a7cedebffd53eff7e936049a89c1ea6b
aldridged <aldridged@gmail.com>
2014-06-11 01:53:44 +0000

    Change Projects create button in accounts to use full form

---
+++ Extension/modules/Accounts/Ext/Layoutdefs/layoutdefs.ext.php
@@ -0,0 +1,7 @@
+<?php
+$layout_defs["Accounts"]["subpanel_setup"]["project"]["top_buttons"] = array(
+  array('widget_class' => 'SubPanelTopCreateButton'),
+  array('widget_class' => 'SubPanelTopSelectButton', 'mode'=>'MultiSelect'),
+  );
+?>
+
