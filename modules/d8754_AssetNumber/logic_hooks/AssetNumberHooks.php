7524743a841b3cab03c7b6acb6427fdb42a010b1
aldridged <aldridged@gmail.com>
2014-06-24 03:01:33 +0000

    Adjusted logic hook code to notif if linked to >=1 rental lines

--- modules/d8754_AssetNumber/logic_hooks/AssetNumberHooks.php
+++ modules/d8754_AssetNumber/logic_hooks/AssetNumberHooks.php
@@ -4,12 +4,12 @@
 class AssetNumberHooks{
     
         function process_record_hook(&$bean, $event, $arguments){
-          $bean->assetnumber_notice_c = '';
+          $bean->d8754_assetnumber_notice_c = '';
           $full_copy = new d8754_AssetNumber();
           $full_copy->retrieve($bean->id);
           $linked_rentalitems = $full_copy->get_linked_beans('d8754_assetnumber_d8753_rentalitem_1','d8753_rentalitem');
-          if(count($linked_rentalitems)>1) {
-            $bean->assetnumber_notice_c = '&lt;span title="Linked to more than one Rental Item" style="color:#FF0000;"&gt;&#x26A0;&lt;/span&gt;';
+          if(count($linked_rentalitems)>=1) {
+            $bean->d8754_assetnumber_notice_c = '&nbsp;<span title="Linked to more than one Rental Item" style="color:#FF0000;">&#x26A0;</span>';
                 };
         }
 }
