f4542b19a89b9d8d772752df8ae2fe2f60d145e4
aldridged@gmail.com <aldridged@gmail.com@1b385842-5835-4f50-da08-b7594bb11436>
2014-06-26 02:38:29 +0000

    Auto number on job number field in Project now works

--- trunk/modules/Project/logic_hooks/ProjectHooks.php
+++ trunk/modules/Project/logic_hooks/ProjectHooks.php
@@ -5,8 +5,9 @@ class ProjectHooks{
    
         function before_save_hook(&$bean, $event, $arguments){
 		  if(empty($bean->fetched_row['id'])) {
-		    $sql = "SELECT MAX(jobnumber_c) FROM project_cstm;";
-            $maxJobnumber = $GLOBALS['db']->getOne($sql);
+		    $db = DBManagerFactory::getInstance();
+		    $sql = "SELECT MAX(jobnumber_c) FROM project_cstm";
+            $maxJobnumber = $db->getOne($sql);
             $maxJobnumber++;
 			$bean->jobnumber_c = $maxJobnumber;
 			};
