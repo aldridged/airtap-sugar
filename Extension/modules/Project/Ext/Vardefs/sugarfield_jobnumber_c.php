741e8fb43154b8ba7bbfce6d530d95aae9719218
aldridged <aldridged@gmail.com>
2014-06-26 02:39:33 +0000

    Remove commas from jobnumber in list views

--- Extension/modules/Project/Ext/Vardefs/sugarfield_jobnumber_c.php
+++ Extension/modules/Project/Ext/Vardefs/sugarfield_jobnumber_c.php
@@ -2,4 +2,5 @@
  // created: 2014-05-29 09:58:12
 $dictionary['Project']['fields']['jobnumber_c']['labelValue']='Job Number';
 $dictionary['Project']['fields']['jobnumber_c']['type']='int';
+$dictionary['Project']['fields']['jobnumber_c']['disable_num_format']='true';
  ?>
