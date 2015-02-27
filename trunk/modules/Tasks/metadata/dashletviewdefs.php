77c791057845d8d59606dec2ad03fc7b1b786436
aldridged@gmail.com <aldridged@gmail.com@1b385842-5835-4f50-da08-b7594bb11436>
2015-02-27 02:53:02 +0000

    Created sugar feed for Tasks module

--- trunk/modules/Tasks/metadata/dashletviewdefs.php
+++ trunk/modules/Tasks/metadata/dashletviewdefs.php
@@ -30,23 +30,17 @@ $dashletData['TasksDashlet']['searchFields'] = array (
   ),
 );
 $dashletData['TasksDashlet']['columns'] = array (
-  'set_complete' => 
-  array (
-    'width' => '1',
-    'label' => 'LBL_LIST_CLOSE',
-    'default' => true,
-    'sortable' => false,
-  ),
   'name' => 
   array (
-    'width' => '40',
+    'width' => '35%',
     'label' => 'LBL_SUBJECT',
     'link' => true,
     'default' => true,
+    'name' => 'name',
   ),
   'parent_name' => 
   array (
-    'width' => '30',
+    'width' => '30%',
     'label' => 'LBL_LIST_RELATED_TO',
     'sortable' => false,
     'dynamic_module' => 'PARENT_TYPE',
@@ -59,61 +53,83 @@ $dashletData['TasksDashlet']['columns'] = array (
       1 => 'parent_type',
     ),
     'default' => true,
+    'name' => 'parent_name',
   ),
   'priority' => 
   array (
-    'width' => '10',
+    'width' => '10%',
     'label' => 'LBL_PRIORITY',
     'default' => true,
+    'name' => 'priority',
   ),
   'status' => 
   array (
-    'width' => '8',
+    'width' => '8%',
     'label' => 'LBL_STATUS',
     'default' => true,
+    'name' => 'status',
   ),
   'date_start' => 
   array (
-    'width' => '15',
+    'width' => '10%',
     'label' => 'LBL_START_DATE',
     'default' => true,
-  ),
-  'time_start' => 
-  array (
-    'width' => '15',
-    'label' => 'LBL_START_TIME',
-    'default' => false,
+    'name' => 'date_start',
   ),
   'date_due' => 
   array (
-    'width' => '15',
+    'width' => '10%',
     'label' => 'LBL_DUE_DATE',
     'default' => true,
+    'name' => 'date_due',
+  ),
+  'set_complete' => 
+  array (
+    'width' => '1%',
+    'label' => 'LBL_LIST_CLOSE',
+    'default' => false,
+    'sortable' => false,
+    'name' => 'set_complete',
+  ),
+  'time_start' => 
+  array (
+    'width' => '15%',
+    'label' => 'LBL_START_TIME',
+    'default' => false,
+    'name' => 'time_start',
   ),
   'date_entered' => 
   array (
-    'width' => '15',
+    'width' => '15%',
     'label' => 'LBL_DATE_ENTERED',
+    'name' => 'date_entered',
+    'default' => false,
   ),
   'date_modified' => 
   array (
-    'width' => '15',
+    'width' => '15%',
     'label' => 'LBL_DATE_MODIFIED',
+    'name' => 'date_modified',
+    'default' => false,
   ),
   'created_by' => 
   array (
-    'width' => '8',
+    'width' => '8%',
     'label' => 'Created by',
     'sortable' => false,
+    'name' => 'created_by',
+    'default' => false,
   ),
   'assigned_user_name' => 
   array (
-    'width' => '8',
+    'width' => '12%',
     'label' => 'LBL_LIST_ASSIGNED_USER',
+    'name' => 'assigned_user_name',
+    'default' => false,
   ),
   'contact_name' => 
   array (
-    'width' => '8',
+    'width' => '8%',
     'label' => 'LBL_LIST_CONTACT',
     'link' => true,
     'id' => 'CONTACT_ID',
@@ -123,5 +139,7 @@ $dashletData['TasksDashlet']['columns'] = array (
     array (
       0 => 'contact_id',
     ),
+    'name' => 'contact_name',
+    'default' => false,
   ),
 );
